a14ce240f3d701ff10c57b91ef7136b369ba6d3a
Christoph Brand <christoph@xcnt.io>
2020-12-31 23:00:04 +0100

    feature(iscp_handler): add locking mechanism

--- modules/esp32_remote/iscp_handler.py
+++ modules/esp32_remote/iscp_handler.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+from uasyncio import Lock
 
 from iscp import discover, eISCP
 
@@ -8,6 +9,7 @@ ISCPCommand = namedtuple("ISCPCommand", ("identifier", "command", "argument"))
 class ISCPHandler:
     def __init__(self):
         self.known_iscps: "Dict[str, eISCP]" = {}
+        self.known_iscps_lock: "Dict[str, Lock]" = {}
 
     async def _update_known_iscps(self) -> None:
         self.known_iscps.update(dict((item.identifier, item) for item in await discover()))
@@ -19,13 +21,16 @@ class ISCPHandler:
     async def send(self, identifier: str, command: str, argument: str) -> "Optional[Tuple[str, str]]":
         print("Sending ISCP command({}, {}={}) to send buffer".format(identifier, command, argument))
         iscp_command = ISCPCommand(identifier, command, argument)
-        iscp = await self._get_or_query(identifier)
-        if iscp is None:
-            print("Didn't find ISCP target with identifier {}".format(iscp))
-            return None
-        print("Found ISCP device for sending to {} ({})".format(identifier, iscp.info))
 
-        return await iscp.command(iscp_command.command, iscp_command.argument)
+        lock = self.known_iscps_lock.setdefault(identifier, Lock())
+        with await lock:
+            iscp = await self._get_or_query(identifier)
+            if iscp is None:
+                print("Didn't find ISCP target with identifier {}".format(iscp))
+                return None
+            print("Found ISCP device for sending to {} ({})".format(identifier, iscp.info))
+
+            return await iscp.command(iscp_command.command, iscp_command.argument)
 
     async def _get_or_query(self, identifier: str) -> "Optional[eISCP]":
         if identifier in self.known_iscps:
